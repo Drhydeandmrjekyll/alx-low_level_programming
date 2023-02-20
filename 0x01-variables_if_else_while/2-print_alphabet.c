64f9c9f09956d7ef6978c368c26a2d8b1177fd48
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-02-20 03:03:47 -0800

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- entry point
+ * description- use putchar to print lowercase alphabets
+ *
+ * return:Always 0 (success)
+ */
+
+int main(void){	
+	char n;
+		for(n = 'a'; n <= 'z'; ++n) {
+			putchar(n);
+		} 
+	return (0);
+}	
+
