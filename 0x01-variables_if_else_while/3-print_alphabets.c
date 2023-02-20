312ff0ab1bac0381b7cf0925d63ba9430353d3e7
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-02-20 10:25:10 -0800

    upper lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- Entry point
+ *
+ * description- print uppercase and lowercase
+ *
+ * return: Always 0 (success)
+ *
+*/
+
+int main(void)
+{	
+	char laf;
+
+	for (laf = 'a'; laf <= 'z'; ++laf)
+		putchar(laf);
+	for (laf ='A'; laf <= 'Z';++laf)
+		putchar(laf);
+	putchar('\n');
+
+return (0);	
+}  
