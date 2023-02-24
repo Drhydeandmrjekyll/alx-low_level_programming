f60a4cafb319a8dc237b4af37be0ebf92e10dbab
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-02-23 16:04:10 -0800

    numbers speak

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_numbers - print 0 - 9
+*             only using _putchar twice
+*
+* Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+int num;
+
+for (num = 0; num <= 9; ++num)
+{
+_putchar(num + 48);
+}
+_putchar('\n');
+}
