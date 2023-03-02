4418419b21e6b044cf1922c28547b56f7367164a
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-03-02 14:12:49 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,12 +6,14 @@
  * Return: negative integer if s1 < s2, 0 if s1 == s2,
  *         positive integer if s1 > s2
  */
+
 int _strcmp(char *s1, char *s2)
 {
-        int i = 0;
+	int i = 0;
 
-        while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
-                i++;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
 
-        return (s1[i] - s2[i]);
+	return (s1[i] - s2[i]);
 }
+
