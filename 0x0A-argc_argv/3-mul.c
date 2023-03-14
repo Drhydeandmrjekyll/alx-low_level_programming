ef5fc47ad330276f90699c4a7492b203d0660a73
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-03-14 08:17:35 -0700

    irony nor sacrasm

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- Program that multiples two arguements passed into it
+ * 
+ * @argc: The number of arguement count
+ * 
+ * @argv: The number of string arguement in array pointer
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+int m;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+m = (atoi(argv[1]) * atoi(argv[2]));
+printf("%d\n", m);
+}
+return (0);
+}
+
