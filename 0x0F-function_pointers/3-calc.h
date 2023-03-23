d198cc548ba19f767d8a5fc0468ea6760f465670
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-03-23 12:57:07 -0700

    aim for aim

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,5 @@
 #ifndef CALC_H
 #define CALC_H
-
 /**
  * struct op - Struct op
  *
@@ -9,10 +8,9 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
-} op_t;
-
+char *op;
+int (*f)(int a, int b);
+}op_t;
 /* Function Prototypes */
 int op_add(int a, int b);
 int op_sub(int a, int b);
