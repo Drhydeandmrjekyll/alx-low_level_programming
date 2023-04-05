cb0de90b15cfd0548df5ced2e8cada1a4d8f3af6
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-04-04 21:21:10 -0700

    node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,14 +1,15 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index- return the node at a certain index 
+ * get_nodeint_at_index- return the node at a certain index
  *                       in linked list
- * @head; first node in linked list
+ *@head: first node in linked list
  *
- * @index: index the node to return
+ *@index: index the node to return
  *
- * Return; pointer to the node searched ot NULL
+ *Return: pointer to the node searched ot NULL
  */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
