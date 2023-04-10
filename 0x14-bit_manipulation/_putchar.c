e18d8f3168ac24f0d4f0eef7038145b78563fc45
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-04-10 14:04:33 -0700

    0.0 converts binary to unsigned int

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unlstd.h>
+
+/**
+ * _putchar - Writes character c to stdout 
+ * @c: Character to print
+ *
+ * Return: On success 1 
+ * On error: return -1 and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return(write(1,&c,1));
+}
+
